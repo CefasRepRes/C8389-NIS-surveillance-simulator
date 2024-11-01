777dbd84e39b4993abe55ad5d2e6c39c8a14b33e
Tom Gibson (Cefas) <tom.gibson@cefas.gov.uk>
2024-11-01 11:22:19 +0000

    Update to ProcessMultipleResults for reviewer comments

--- functions/ProcessMultipleResults.R
+++ functions/ProcessMultipleResults.R
@@ -1,17 +1,20 @@
 #' 09/06/2023
 #' Tom Gibson
 #' ProcessMultipleResults
-#' checked 09/06/2023
+#' checked 09/06/2023 Updated on 01/11/2024
 
 #' A helper function to process the results of runSurveillanceSimulation where multiple sites are sampled. 
 #' It groups over each simulation and gets the required summary statistic across sites. 
 
 #' @param result.df a results data.frame from runSurveillanceSimulation
-#' @param detection.summary method for detection summary statistic - mean, median, first and last
+#' @param detection.summary method for detection summary statistic - mean, median, first and last, 
+#' or time.to.detect.n i.e. return the time it took to detect the n'th site with sites ordered by time to detection
+#' you must then specify time.to.detect.n i.e. for which site is the detection time returned. 
 
 #' @return final a processed vector of results, one element per simulation 
 
-ProcessMultipleResults <- function(result.df = resultsA, detection.summary = "last", create.plot = F){
+ProcessMultipleResults <- function(result.df = resultsA, detection.summary = "last", create.plot = F,
+                                   time.to.detect.n = 1){
   
   # Generate Summary Information
   if(detection.summary == "mean"){ # get the mean time to detection per simulation
@@ -34,6 +37,11 @@ ProcessMultipleResults <- function(result.df = resultsA, detection.summary = "la
     result.df %>% group_by(sim_n) %>% summarise(dtime = max(dtime)) -> final.dat
     final.dat$dtime -> final
 
+  }else if(detection.summary == "time.to.detect.n"){
+    
+    result.df %>% group_by(sim_n) %>% arrange(dtime, .by_group = T) %>% slice(time.to.detect.n) -> final.dat
+    final.dat$dtime -> final
+    
   }else{return(print("Error: Wrongly specified process option"))}
   
   # Create Plot 
